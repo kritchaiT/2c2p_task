7339936788dcfe42bb5c47e1edc9b6eec3a22d81
kritchaiT <89719947+kritchaiT@users.noreply.github.com>
2026-01-15 12:01:28 +0000

    initialized 20260115

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # 2c2p_task
+
+Start by running the "Main.java" as an entry point
---
+++ src/Transaction.java
@@ -0,0 +1,5 @@
+public record Transaction(String id, double amount, String status) {
+    // That's it! 
+    // Java automatically builds the constructor, getters, 
+    // equals(), hashCode(), and toString() for you.
+}
