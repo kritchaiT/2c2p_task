005ab245b64ac81e1698f075a76d56da61ac53c0
kritchaiT <89719947+kritchaiT@users.noreply.github.com>
2026-01-16 06:50:18 +0000

    adding validation result as return

--- transaction-analysis/src/main/java/com/example/App.java
+++ transaction-analysis/src/main/java/com/example/App.java
@@ -4,7 +4,9 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class App 
 {
@@ -26,14 +28,22 @@ public class App
             new File(filePath),
             new TypeReference<List<Transaction>>() {}
             );
-            
+
             displayDataShowcase(transactions);
 
         } catch (IOException e) {
             System.err.println("Error reading the JSON file: " + e.getMessage());
         }
 
-        validateTransactions(transactions);
+        // 1. Call the method and store it in a Map
+        Map<String, List<Transaction>> result = validateTransactions(transactions);
+
+        // 2. Access the lists using the .get() method and the keys "valid" and "invalid"
+        int validCount = result.get("valid").size();
+        int invalidCount = result.get("invalid").size();
+
+        System.out.println("Valid count: " + validCount);
+        System.out.println("Invalid count: " + invalidCount);
     }
 
 // Method to display data showcase
@@ -61,21 +71,24 @@ public class App
         System.out.println("====================================================");
     }
 
-    // Method to validate transactions
-    public static void validateTransactions(List<Transaction> list) {
-
+    // Method to validate transactions -> using Map to conatin valid and invalid transactions
+    public static Map<String, List<Transaction>> validateTransactions(List<Transaction> rawTxn) {
     List<Transaction> validTxns = new ArrayList<>();
     List<Transaction> invalidTxns = new ArrayList<>();
 
-    for (Transaction tx : list) {
+    for (Transaction tx : rawTxn) {
         if (Validation.isValidTransaction(tx)) {
             validTxns.add(tx);
         } else {
-            System.out.println("Invalid transaction found: " + tx);
             invalidTxns.add(tx);
         }
     }
+
+    // using hashmap to store valid and invalid transactions
+    Map<String, List<Transaction>> result = new HashMap<>();
+    result.put("valid", validTxns);
+    result.put("invalid", invalidTxns);
+    
+    return result;
 }
-
-
 }
